500fdb5013342c4ef8efaf95d7ca194dcf3e9a14
David Oury <david.oury@playplay.com>
2025-03-01 17:31:03 +0100

    fix: simplify best sellers feature test

--- tests/Feature/Http/GetBestSellers/GetBestSellersControllerTest.php
+++ tests/Feature/Http/GetBestSellers/GetBestSellersControllerTest.php
@@ -8,22 +8,22 @@ use Tests\TestCase;
 
 class GetBestSellersControllerTest extends TestCase
 {
-    private const string ENDPOINT_URL = 'api/best-sellers';
+    private const string ENDPOINT_URL = '/api/best-sellers';
 
     public function testItReturnsResponseWithStatusOkWhenGivenParametersAreValid(): void
     {
-        $url = self::ENDPOINT_URL . '?author=Isaac&isbn[]=0553293389&isbn[]=9780553293388&title=Whatever&offset=20';
-
-        $response = $this->json('GET', $url);
+        $response = $this->getJson(
+            self::ENDPOINT_URL . '?author=Isaac&isbn[]=0553293389&isbn[]=9780553293388&title=Whatever&offset=20',
+        );
         $response->assertStatus(200);
         $response->assertJson(['foo' => 'bar']);
     }
 
     public function testItReturnsValidationErrorsWhenGivenParameterAreInvalid(): void
     {
-        $url = self::ENDPOINT_URL . '?isbn[]=055329338&isbn[]=97805532933888&offset=10';
-
-        $response = $this->json('GET', $url);
+        $response = $this->getJson(
+            self::ENDPOINT_URL . '?isbn[]=055329338&isbn[]=97805532933888&offset=10',
+        );
         $response->assertStatus(422);
         $response->assertJson([
             'errors' => [
