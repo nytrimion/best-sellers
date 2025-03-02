ae212ac8ec2406e32504e5189d7ebf14db137df6
David Oury <david.oury@playplay.com>
2025-03-02 10:37:37 +0100

    feat: cleanup get best sellers request test

--- tests/Feature/Http/GetBestSellers/GetBestSellersRequestTest.php
+++ tests/Feature/Http/GetBestSellers/GetBestSellersRequestTest.php
@@ -20,32 +20,42 @@ class GetBestSellersRequestTest extends TestCase
         $this->sut  = new GetBestSellersRequest();
     }
 
+    /**
+     * @return iterable<string, array{
+     *     parameters: array<string, mixed>,
+     * }>
+     */
     static public function validParametersProvider(): iterable
     {
-        yield 'no parameters' => [[]];
-        yield 'author' => [[
-            'author' => 'John',
-        ]];
-        yield 'isbn 10' => [[
-            'isbn' => ['0553293389'],
-        ]];
-        yield 'isbn 13' => [[
-            'isbn' => ['9780553293388'],
-        ]];
-        yield 'isbn list' => [[
-            'isbn' => ['0553293389', '9780553293388'],
-        ]];
-        yield 'title' => [[
-            'title' => 'whatever',
-        ]];
-        yield 'offset 0' => [[
-            'offset' => '0',
-        ]];
-        yield 'offset 20' => [[
-            'offset' => '20',
-        ]];
+        yield 'no parameters' => [
+            'parameters' => [],
+        ];
+        yield 'author' => [
+            'parameters' => ['author' => 'John'],
+        ];
+        yield 'isbn 10' => [
+            'parameters' => ['isbn' => ['0553293389']],
+        ];
+        yield 'isbn 13' => [
+            'parameters' => ['isbn' => ['9780553293388']],
+        ];
+        yield 'isbn list' => [
+            'parameters' => ['isbn' => ['0553293389', '9780553293388']],
+        ];
+        yield 'title' => [
+            'parameters' => ['title' => 'whatever'],
+        ];
+        yield 'offset 0' => [
+            'parameters' => ['offset' => '0'],
+        ];
+        yield 'offset 20' => [
+            'parameters' => ['offset' => '20'],
+        ];
     }
 
+    /**
+     * @param array<string, mixed> $parameters
+     */
     #[DataProvider('validParametersProvider')]
     public function testItPassesValidation(array $parameters): void
     {
@@ -54,40 +64,48 @@ class GetBestSellersRequestTest extends TestCase
         $this->assertTrue($validator->passes());
     }
 
+    /**
+     * @return iterable<string, array{
+     *     parameters: array<string, mixed>,
+     * }>
+     */
     static public function invalidParametersProvider(): iterable
     {
-        yield 'isbn 10 invalid' => [[
-            'isbn' => ['5553293389'],
-        ]];
-        yield 'isbn 13 invalid' => [[
-            'isbn' => ['7780553293388'],
-        ]];
-        yield 'isbn 9' => [[
-            'isbn' => '006137422',
-        ]];
-        yield 'isbn 11' => [[
-            'isbn' => '00613742299',
-        ]];
-        yield 'isbn 12' => [[
-            'isbn' => '978044657993',
-        ]];
-        yield 'isbn 14' => [[
-            'isbn' => '97804465799333',
-        ]];
-        yield 'offset -1' => [[
-            'offset' => '-1',
-        ]];
-        yield 'offset 1' => [[
-            'offset' => '1',
-        ]];
-        yield 'offset 19' => [[
-            'offset' => '19',
-        ]];
-        yield 'offset 21' => [[
-            'offset' => '21',
-        ]];
+        yield 'isbn 10 invalid' => [
+            'parameters' => ['isbn' => ['5553293389']],
+        ];
+        yield 'isbn 13 invalid' => [
+            'parameters' => ['isbn' => ['7780553293388']],
+        ];
+        yield 'isbn 9' => [
+            'parameters' => ['isbn' => '006137422'],
+        ];
+        yield 'isbn 11' => [
+            'parameters' => ['isbn' => '00613742299'],
+        ];
+        yield 'isbn 12' => [
+            'parameters' => ['isbn' => '978044657993'],
+        ];
+        yield 'isbn 14' => [
+            'parameters' => ['isbn' => '97804465799333'],
+        ];
+        yield 'offset -1' => [
+            'parameters' => ['offset' => '-1'],
+        ];
+        yield 'offset 1' => [
+            'parameters' => ['offset' => '1'],
+        ];
+        yield 'offset 19' => [
+            'parameters' => ['offset' => '19'],
+        ];
+        yield 'offset 21' => [
+            'parameters' => ['offset' => '21'],
+        ];
     }
 
+    /**
+     * @param array<string, mixed> $parameters
+     */
     #[DataProvider('invalidParametersProvider')]
     public function testItFailsValidation(array $parameters): void
     {
